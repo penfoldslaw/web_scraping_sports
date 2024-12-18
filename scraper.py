e77fe26fb48aac6e4f84f83121dc3a6f3b00fb91
Andy Amoah Mensah <122259813+penfoldslaw@users.noreply.github.com>
2024-12-18 21:39:36 +0000

    figured out how to put data into list

--- scraper.py
+++ scraper.py
@@ -7,6 +7,9 @@ html_content = response.content
 
 #print(html_content)
 
+with open('nba_html_2024.html', 'r', encoding='utf-8') as file:
+    html_content = file.read()
+
 
 soup = BeautifulSoup(html_content, 'html.parser')
 table = soup.find('table', class_='Crom_table__p1iZz')
@@ -14,4 +17,31 @@ table = soup.find('table', class_='Crom_table__p1iZz')
 #headers = [header.text for header in table.find_all('th')]
 # for heading in headings:
 #     print(heading.text)
-print(soup[:100])
+
+
+# Parse the HTML content with BeautifulSoup
+soup = BeautifulSoup(html_content, 'html.parser')
+
+# Find the table
+table = soup.find('table', class_='Crom_table__p1iZz')
+
+# Extract headers
+headers = [header.text.strip() for header in table.find_all('th')]
+
+# stats = [headers.text.strip() for headers in table.find_all('a', href='/stats/')]
+stats = [headers.text.strip() for headers in table.find_all('td')]
+
+tr_elements = soup.find_all('tr')
+
+
+giannis_tag = soup.find('a', href='/stats/player/203507/')
+
+
+
+table_data = []
+for tr in tr_elements:
+    row_data = [td.get_text(strip=True) for td in tr.find_all('td')]
+    table_data.append(row_data)
+
+
+print(table_data)

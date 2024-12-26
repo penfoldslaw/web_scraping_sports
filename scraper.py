943ca7bae1dc4499ba20b9e23fc25d25400507ba
Andy Mensah <122259813+penfoldslaw@users.noreply.github.com>
2024-12-25 20:59:00 -0500

    scraper code for one player done

--- scraper.py
+++ scraper.py
@@ -8,23 +8,38 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 import os
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.options import Options
+from webdriver_manager.firefox import GeckoDriverManager
 
-# Set up Chrome options
-chrome_options = Options()
-chrome_options.add_argument("--start-maximized")  # Example option: Start maximized
-chrome_options.add_argument("--disable-popup-blocking")
+# Chrome options
+# chrome_options = Options()
+# chrome_options.add_argument("--start-maximized")  # Example option: Start maximized
+# chrome_options.add_argument("--disable-popup-blocking")
 
-# Initialize the WebDriver
-driver = webdriver.Chrome(
-    service=Service(ChromeDriverManager().install()), 
-    options=chrome_options
+# # Initialize the WebDriver
+# driver = webdriver.Chrome(
+#     service=Service(ChromeDriverManager().install()), 
+#     options=chrome_options
+# )
+
+
+#  Firefox options
+firefox_options = Options()
+# firefox_options.add_argument("--headless")  # Run in headless mode if needed
+firefox_options.add_argument("--start-maximized")  # Run in headless mode if needed
+
+# Initialize the Firefox WebDriver
+driver = webdriver.Firefox(
+    service=Service(GeckoDriverManager().install()),
+    options=firefox_options
 )
 
 
 
 
 
-def scrape_data(player,year):
+def scrape_data(player,season,folder_year):
     driver.get("https://www.nba.com/players")
     time.sleep(2)
 
@@ -47,39 +62,42 @@ def scrape_data(player,year):
     player_link = driver.find_element(By.XPATH, "//div[@class='RosterRow_playerName__G28lg']")
     player_link.click()  # This simulates a click
 
-    time.sleep(2)
+    #time.sleep(2)
 
 
     #this click the stats page 
     stats_link = driver.find_element(By.XPATH, "//ul[@class='InnerNavTabs_list__tIFRN']/li[2]")
     stats_link.click()
 
-    time.sleep(2)
+    #time.sleep(2)
 
     # Locate and click the dropdown button for the advance stats page
-    dropdown_button = driver.find_element(By.CLASS_NAME, "ArrowToggleButton_arrowText__ep3Dn")
+    dropdown_button = driver.find_element(By.XPATH, "//*[@id='__next']/div[2]/div[2]/section/div[4]/section[1]/div/nav/div[1]/button/span")
+    driver.execute_script("window.scrollBy(0, 200);")  # Scroll down by 500 pixels
+
     dropdown_button.click()
-    time.sleep(2)
+    #time.sleep(2)
 
     # Locate and click the "Career" option
     advance_option = driver.find_element(By.LINK_TEXT, 'Advanced Box Scores')
-    #scroll to the advance_option to prevent the code from breaking
-    driver.execute_script("window.scrollBy(0, 500);")  # Scroll down by 500 pixels
-    ## another option if this keeps breaking is to find the button for advance click it so the option will become avaliable and click on that
     advance_option.click()
 
     # Add another brief delay to allow the dropdown to expand
     time.sleep(2)
 
     # this clicks on the dropdown for the season
-    dropdown_season = driver.find_element(By.XPATH, "//div[@class='DropDown_dropdown__TMlAR']")
+    dropdown_season = driver.find_element(By.XPATH, "//*[@id='__next']/div[2]/div[2]/section/div[4]/section[2]/div/div/div[1]/label/div")
+    driver.execute_script("window.scrollBy(0, 200);")  #this is to scroll down
+
     dropdown_season.click()
     time.sleep(2)
 
     #this click on the season that you want
-    season_year = year
-    season = driver.find_element(By.XPATH,  f"//option[@value={season_year}]")
-    season.click()
+    season_year = season
+    season_select = driver.find_element(By.XPATH,  f"//option[@value={season_year}]")
+    season_select.click()
+    #driver.execute_script("window.scrollBy(0, 300);") 
+
 
     time.sleep(2)
 
@@ -88,18 +106,66 @@ def scrape_data(player,year):
 
 
     # Save the HTML to a file
-    folder = os.path.join("nba_historic", f"nba_html_{season_year}")
+ 
+    folder = os.path.join("nba_historic", f"nba_html_{folder_year}")
     file_path = os.path.join(folder, f"{player}_content.html")
     with open(file_path, "w", encoding="utf-8") as file:
         file.write(page_html)
 
-    # time.sleep(30)
+    #time.sleep(30)
+    print("All quarters printed")
+    # advance stats  
+    advance_filter = driver.find_element(By.XPATH, "//*[@id='__next']/div[2]/div[2]/section/div[4]/section[2]/div/div/div[4]/div[2]/button/span")
+    advance_filter.click()
+
+    time.sleep(2)
+
+    #this select the quarters button
+    select_quaters = driver.find_element(By.XPATH,"//*[@id='__next']/div[2]/div[2]/section/div[4]/section[2]/div/div/div[4]/div[1]/section[2]/div/div[3]/label/div")
+    # driver.execute_script("arguments[0].scrollIntoView(true);", select_quaters)
+    select_quaters.click()
+
+    #this select q1
+
+    xpath_quarters=["//*[@id='__next']/div[2]/div[2]/section/div[4]/section[2]/div/div/div[4]/div[1]/section[2]/div/div[3]/label/div/select/option[2]",
+                    "//*[@id='__next']/div[2]/div[2]/section/div[4]/section[2]/div/div/div[4]/div[1]/section[2]/div/div[3]/label/div/select/option[3]",
+                    "//*[@id='__next']/div[2]/div[2]/section/div[4]/section[2]/div/div/div[4]/div[1]/section[2]/div/div[3]/label/div/select/option[4]",
+                    "//*[@id='__next']/div[2]/div[2]/section/div[4]/section[2]/div/div/div[4]/div[1]/section[2]/div/div[3]/label/div/select/option[5]"]
+    
+    folder_quarter_data = ["q1","q2", "q3", "q4"]
+    
+    for nba_quarter, folder_name in zip(xpath_quarters, folder_quarter_data):
+        print(f"Processing: {nba_quarter}, {folder_name}")
+
+        select_q1 = driver.find_element(By.XPATH, nba_quarter)
+        select_q1.click()
+
+        get_results = driver.find_element(By.XPATH, "//*[@id='__next']/div[2]/div[2]/section/div[4]/section[2]/div/div/div[4]/div[2]/div/button[2]")
+        get_results.click()
+
+
+        page_html = driver.page_source
+
+
+
+
+        # Save the HTML to a file
+        which_q_folder = folder_name
+        folder = os.path.join("nba_historic", f"nba_html_{folder_year}", "quarter_data", which_q_folder)
+        file_path = os.path.join(folder, f"{player}_content_{which_q_folder}.html")
+        with open(file_path, "w", encoding="utf-8") as file:
+            file.write(page_html)
+
+
+
+    
+    # time.sleep(10)
 
     print(driver.title.encode('ascii', 'replace').decode())
     driver.quit()
 
 if __name__ == "__main__":
-    scrape_data("Paul George",'"2019-20"')
+    scrape_data("Paul George",'"2019-20"', "2019")
 
 
 

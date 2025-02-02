4f7f6af312383110b8b913016f937a6e333a1e4c
Andy Mensah <122259813+penfoldslaw@users.noreply.github.com>
2025-02-02 13:26:47 -0500

    Stop tracking current_logs directory

--- historic_defense_data/his_defense_parser_processor.py
+++ historic_defense_data/his_defense_parser_processor.py
@@ -12,3 +12,23 @@ if __name__ == "__main__":
         run_script(c_folder, csv_s_folder)
     
     print("All scripts have finished executing.")
+
+
+
+import sys
+from pathlib import Path
+from his_defense_parser import defense_parser # Assuming this is the function inside the script
+
+def run_script(sub_folder, csv_sub_folder):
+    # Define the path of the subfolder or any other required parameters.
+    #path = Path().resolve()
+    defense_parser(sub_folder, csv_sub_folder)  # Call the function directly
+
+if __name__ == "__main__":
+    sub_folder = ["nba_html_2022-23", "nba_html_2023-24"] 
+    csv_sub_folder = ["defense_csv_2022-23", "defense_csv_2023-24"]
+
+    for c_folder, csv_s_folder in zip(sub_folder, csv_sub_folder):
+        run_script(c_folder, csv_s_folder)
+    
+    print("All scripts have finished executing.")
--- historic_defense_data/his_defense_scarper_processor.py
+++ historic_defense_data/his_defense_scarper_processor.py
@@ -1,17 +1,38 @@
-import subprocess
-import sys
+# import subprocess
+# import sys
+
+# def run_script(main_folder, folder_season, data_season):
+#     # Pass arguments to defense_scraper.py
+#     subprocess.run(
+#         [sys.executable, "defense_scraper.py", main_folder, folder_season, data_season],
+#         check=True
+#     )
+
+# if __name__ == "__main__":
+#     main_folder = "D:/nba_defense_historic"
+#     folder_season = ["2019-20", "2020-21", "2021-22", "2022-23", "2023-24"]
+#     data_season = ["2019-20", "2020-21", "2021-22", "2022-23", "2023-24"]
+
+#     for folder, data in zip(folder_season, data_season):
+#         print(f"Running script for: folder_season={folder}, data_season={data}")
+#         run_script(main_folder, folder, data)
+    
+#     print("All scripts have finished executing.")
+
+
+
+
+from pathlib import Path
+from his_defense_scraper import defense_scraper  # Assuming this is the function inside the script
 
 def run_script(main_folder, folder_season, data_season):
-    # Pass arguments to defense_scraper.py
-    subprocess.run(
-        [sys.executable, "defense_scraper.py", main_folder, folder_season, data_season],
-        check=True
-    )
+    # Directly call the function from the other script
+    defense_scraper(main_folder, folder_season, data_season)
 
 if __name__ == "__main__":
     main_folder = "D:/nba_defense_historic"
-    folder_season = ["2019-20", "2020-21", "2021-22", "2022-23", "2023-24"]
-    data_season = ["2019-20", "2020-21", "2021-22", "2022-23", "2023-24"]
+    folder_season = ["2022-23", "2023-24"]
+    data_season = ["2022-23", "2023-24"]
 
     for folder, data in zip(folder_season, data_season):
         print(f"Running script for: folder_season={folder}, data_season={data}")

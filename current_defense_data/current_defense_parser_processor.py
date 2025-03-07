1b4949196daf451935aa3e9006ae1afff2aac27d
Andy Mensah <122259813+penfoldslaw@users.noreply.github.com>
2025-03-06 21:58:40 -0500

    dataframe in pred ipbyn

--- current_defense_data/current_defense_parser_processor.py
+++ current_defense_data/current_defense_parser_processor.py
@@ -17,21 +17,3 @@ if __name__ == "__main__":
     
     print("All scripts have finished executing.")
 
-
-# import sys
-# from pathlib import Path
-# from current_defense_parser import defense_parser # Assuming this is the function inside the script
-
-# def run_script(sub_folder, csv_sub_folder):
-#     # Define the path of the subfolder or any other required parameters.
-#     #path = Path().resolve()
-#     defense_parser(sub_folder, csv_sub_folder)  # Call the function directly
-
-# if __name__ == "__main__":
-#     sub_folder = ["nba_html_2024-25"]
-#     csv_sub_folder = ["defense_csv_2024-25"]
-
-#     for c_folder, csv_s_folder in zip(sub_folder, csv_sub_folder):
-#         run_script(c_folder, csv_s_folder)
-    
-#     print("All scripts have finished executing.")
